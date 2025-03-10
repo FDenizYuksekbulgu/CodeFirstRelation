70bc7c2d1be39cb9cd33b84c89e036de69e88ec4
Deniz Yuksekbukgu <fdenizyuksekbulgu@gmail.com>
2025-03-10 16:21:44 +0300

    Add project files.

---
+++ CodeFirstRelation/Entities/UserEntity.cs
@@ -0,0 +1,14 @@
+﻿,namespace CodeFirstRelation.Entities
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+
+
+        //Relational Property
+
+        public List<PostEntity> Posts { get; set; }
+    }
+}
